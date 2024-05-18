2b2820966279401945966689d4620707084031b4
ValdiF11 <valdifirstianto@gmail.com>
2024-05-18 21:59:22 +0700

    fix: wiring item detail, midtrans & response

--- Check-In/src/screens/Home/MidtransWebview.jsx
+++ Check-In/src/screens/Home/MidtransWebview.jsx
@@ -1,15 +1,46 @@
 import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
-import { StyleSheet } from "react-native";
+import { StyleSheet, ActivityIndicator } from "react-native";
 
-export default function MidtransWebView() {
-  return <WebView style={styles.container} source={{ uri: "https://expo.dev" }} />;
-  // tinggal ganti urinya pake midtrans
+export default function MidtransWebView({ navigation, route }) {
+  const { url } = route.params;
+
+  const handleNavigationStateChange = (navState) => {
+    // Check if the web view has finished loading
+    if (!navState.loading) {
+      // Check if the URL is the completion URL
+      if (navState.url === `${url}#/success`) {
+        // Navigate to another screen
+        navigation.navigate("InvoiceResponse");
+      } else if (navState.url === `${url}#/failed`) {
+        navigation.navigate("/");
+      }
+    }
+  };
+
+  return (
+    <WebView
+      style={styles.container}
+      source={{ uri: url }}
+      onLoad={() => console.log("WebView loaded")}
+      onNavigationStateChange={handleNavigationStateChange}
+      renderLoading={() => <ActivityIndicator style={styles.loadingIndicator} />}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
+  },
+  loadingIndicator: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
